4d01adc74d92cfcca5922dc2ebe3404500af9da4
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-04-24 15:01:33 +0200

    Consegna

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public interface Inuotante
+    {
+        public void Nuota()
+        {
+            Console.WriteLine($"Ciao, sono un {GetType().Name} e sto nuotando!!!");
+        }
+    }
+    public interface IVolante
+    {
+        public void Vola()
+        {
+            Console.WriteLine($"Ciao, sono un {GetType().Name} e sto volando!!!!");
+        }
+    }
+    public abstract class Animale
+    {
+        public void IoSono()
+        {
+            Console.WriteLine($"Ciao io sono {GetType().Name}");
+        }
+        public void Dormi()
+        {
+            Console.WriteLine("Zzzzz");
+        }
+
+        public abstract void Mangia();
+
+        public abstract void Verso();
+    }
+    public class Cane : Animale, Inuotante
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine($"Sto mangiando carne e crocchette");
+        }
+        public override void Verso()
+        {
+            Console.WriteLine("Bau Bau");
+        }
+    }
+    public class Passerotto : Animale, IVolante
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Sto mangiando la semenza");
+        }
+        public override void Verso()
+        {
+            Console.WriteLine("Cip Cip");
+        }
+    }
+    public class Aquila : Animale, IVolante
+    {
+        public override void Mangia()
+        {
+            Console.WriteLine("Sto mangiando la carne");
+        }
+
+        public override void Verso()
+        {
+            Console.WriteLine("Sto stridendo");
+        }
+
+    }
+    public class Delfino : Animale, Inuotante
+    {
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Sto mangiando il pesce");
+        }
+        public override void Verso()
+        {
+            Console.WriteLine("Sto fischettando");
+        }
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace csharp_abstract_animals
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.RegularExpressions;
+using System;
+using System.Runtime.InteropServices;
+
+namespace csharp_abstract_animals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            **Esercizio 1 : classi astratte*
+            */
+            List<Animale> animali = new List<Animale>();
+            animali.Add(new Aquila());
+            animali.Add(new Passerotto());
+            animali.Add(new Delfino());
+            animali.Add(new Cane());
+            foreach(Animale animale in animali)
+            {
+                Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/");
+                animale.IoSono();
+                animale.Verso();
+                animale.Mangia();
+                animale.Dormi();
+            }
+            /*
+            **Esercizio 2 : interfacce*
+            */
+            List<IVolante> volatili = new List<IVolante>() { new Passerotto() , new Aquila()};
+            foreach(IVolante animaleVolatile in volatili)
+            {
+                animaleVolatile.Vola();
+            }
+            List<Inuotante> natanti = new List<Inuotante>() { new Cane(), new Delfino()};
+            foreach (Inuotante animaleNatante in natanti)
+            {
+                animaleNatante.Nuota();
+            }
         }
+        static void FaiVolare(Inuotante animale)
+        {
+            animale.Nuota();
+        }
+        static void FaiNuotare(IVolante animale)
+        {
+            animale.Vola();
+        }
+
     }
 }
